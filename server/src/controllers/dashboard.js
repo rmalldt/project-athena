e381512d04b93b7e35dae4a8c41b2dc931fbe18c
rmalldt <rmall.dt@gmail.com>
2025-04-22 15:53:11 +0100

    Update Dashboard controller

--- server/src/controllers/dashboard.js
+++ server/src/controllers/dashboard.js
@@ -1,7 +1,20 @@
 async function dashboard(req, res) {
+  if (!req.user) {
+    return res.status(403).json({ error: 'Not Authenticated' });
+  }
+
+  const user = req.user;
   try {
     console.log('USER: ', req.user);
-    res.status(200).json({ success: true, data: 'Dashboard' });
+    res.status(200).json({
+      success: true,
+      data: {
+        student_id: user.student_id,
+        username: user.username,
+        email: user.email,
+      },
+      message: 'Dashboard',
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
