92ec42a815aefda5dda3469dee85680c57ad75c3
“Shaymo97” <shaymohajeri@gmail.com>
2025-04-23 15:20:00 +0100

    cleanup index.test.js

--- client/__test__/index.test.js
+++ client/__test__/index.test.js
@@ -5,8 +5,6 @@ let dom;
 let document;
 
 describe('index.html', () => {
-    // Before each test set virtual dom so we can use the document property to
-    // perform our tests.
     beforeEach(async () => {
       dom = await renderDOM('./index.html');
       document = await dom.window.document;
@@ -62,49 +60,6 @@ describe('index.html', () => {
         expect(actual).toBe(expected);
   
       });
-  
-    // it('displays dark mode', () => {
-    //   // Arrange
-    //   const body = document.querySelector('body');
-    //   const darkModeBtn = document.querySelector('#dark-mode');
-  
-    //   // Act
-    //   darkModeBtn.click();
-  
-    //   // Assert
-    //   expect(body.className).toBe('dark-mode');
-    // });
-  
-    // it('adds the input value to the h1', () => {
-    //   // Arrange
-    //   const form = document.querySelector('form');
-    //   const h1 = document.querySelector('h1');
-    //   const input = document.querySelector('#name');
-    //   input.value = 'emile';
-  
-    //   // Act
-    //   form.dispatchEvent(new dom.window.Event('submit'));
-  
-    //   // Assert
-    //   expect(h1.innerHTML).toContain(input.value);
-    // });
-  
-    // it('switches back to light mode', () => {
-    //   // Arrange
-    //   const body = document.querySelector('body');
-    //   const darkModeBtn = document.querySelector('#dark-mode');
-  
-    //   // Act
-    //   darkModeBtn.click();
-    //   darkModeBtn.click();
-  
-    //   // Assert
-    //   expect(body.className).toBe('');
-    // });
-  
-    // it('test', () => {
-    //   expect({ id: 1, name: 'Kale' }).toEqual({ id: 1, name: 'Kale' });
-    // });
   });
    
 
