0ab2bc4b87752496defb90efb88aaa602d5a1fa6
“Shaymo97” <shaymohajeri@gmail.com>
2025-04-23 16:37:59 +0100

    update dashboard.js to clear local storage when clicking logout

--- client/js/dashboard.js
+++ client/js/dashboard.js
@@ -1,9 +1,18 @@
-// immediately invovled function call
+const logoutButton = document.querySelector('#logout');
 
-// const dashboardResponse = await fetch('http://localhost:3000/users/logout', {
-// method: 'DELETE',
-// headers: headerslist,
-//   });
+async function logout(e) {
+    console.log("hit")
+    e.preventDefault();
 
-// localStorage.clear()
-// window.location.href = './index.html';
+    const headerslist = localStorage
+    
+    const dashboardResponse = await fetch('http://localhost:3000/users/logout', {
+    method: 'DELETE',
+    headers: headerslist,
+      });
+
+    localStorage.clear()
+    window.location.href = './index.html';
+}
+
+logoutButton.addEventListener('click', logout);
