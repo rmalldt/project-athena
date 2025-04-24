5fbd6aa5859e5c0f8d2e634bf9215b8333fda54d
Gordana <goca.perun1@gmail.comgit config --global user.email goca.perun1@gmail.comgit config --global user.email goca.perun1@gmail.com>
2025-04-24 11:14:50 +0100

    Update Topic router to reflect changes in topic model

--- server/src/routers/topic.js
+++ server/src/routers/topic.js
@@ -1,31 +1,13 @@
 const express = require('express');
-const { body, query } = require('express-validator');
 const topicController = require('../controllers/topic')
 const router = express.Router();
 
-router.get('/search/title',
-    [
-        query('title', 'title query parameter is required').notEmpty()
-    ],
-     topicController.searchByTitle);
 
-router.get('/search/description', 
-    [
-        query('description', 'description query parameter is required').notEmpty()
-    ],
-    topicController.searchByDescription);
-
+// List all topics
 router.get('/', topicController.getAll);  
 
+// Get a topic by ID
 router.get('/:id', topicController.getById); 
-        
-router.post('/',
-    [
-        body('title', 'Title is required').notEmpty(),
-        body('videoUrl', 'videoUrl is required').notEmpty(),
-        body('videoUrl', 'videoUrl must be a valid URL').isURL(),
-        body('description').optional()
-    ],
-    topicController.create);           
+                
 
 module.exports = router;
